32e1d7b11055d14263cf8f186ca0b410b961788a
Yongduek Seo <yndk@sogang.ac.kr>
2015-03-23 14:43:17 +0900

    Changes to be committed: 	modified:   CameraCalibrator.cpp  	modified:   CameraCalibrator.h  	modified:   Makefile  	modified:   calibrate.cpp  	new file:   chessboards2/left01.jpg  	new file:   chessboards2/left02.jpg  	new file:   chessboards2/left03.jpg  	new file:   chessboards2/left04.jpg  	new file:   chessboards2/left05.jpg  	new file:   chessboards2/left06.jpg  	new file:   chessboards2/left07.jpg  	new file:   chessboards2/left08.jpg  	new file:   chessboards2/left09.jpg  	new file:   chessboards2/left11.jpg  	new file:   chessboards2/left12.jpg  	new file:   chessboards2/left13.jpg  	new file:   chessboards2/left14.jpg

--- SourceCodes/Chapter 09/CameraCalibrator.cpp
+++ SourceCodes/Chapter 09/CameraCalibrator.cpp
@@ -1,138 +1,197 @@
 /*------------------------------------------------------------------------------------------*\
-   This file contains material supporting chapter 9 of the cookbook:  
-   Computer Vision Programming using the OpenCV Library. 
-   by Robert Laganiere, Packt Publishing, 2011.
-
-   This program is free software; permission is hereby granted to use, copy, modify, 
-   and distribute this source code, or portions thereof, for any purpose, without fee, 
-   subject to the restriction that the copyright notice may not be removed 
-   or altered from any source or altered source distribution. 
-   The software is released on an as-is basis and without any warranties of any kind. 
-   In particular, the software is not guaranteed to be fault-tolerant or free from failure. 
-   The author disclaims all warranties with regard to this software, any use, 
-   and any consequent failure, is purely the responsibility of the user.
+ This file contains material supporting chapter 9 of the cookbook:
+ Computer Vision Programming using the OpenCV Library.
+ by Robert Laganiere, Packt Publishing, 2011.
  
-   Copyright (C) 2010-2011 Robert Laganiere, www.laganiere.name
-\*------------------------------------------------------------------------------------------*/
+ This program is free software; permission is hereby granted to use, copy, modify,
+ and distribute this source code, or portions thereof, for any purpose, without fee,
+ subject to the restriction that the copyright notice may not be removed
+ or altered from any source or altered source distribution.
+ The software is released on an as-is basis and without any warranties of any kind.
+ In particular, the software is not guaranteed to be fault-tolerant or free from failure.
+ The author disclaims all warranties with regard to this software, any use,
+ and any consequent failure, is purely the responsibility of the user.
+ 
+ Copyright (C) 2010-2011 Robert Laganiere, www.laganiere.name
+ \*------------------------------------------------------------------------------------------*/
 
 #include "CameraCalibrator.h"
 
 // Open chessboard images and extract corner points
 int CameraCalibrator::addChessboardPoints(
-         const std::vector<std::string>& filelist, 
-         cv::Size & boardSize) {
-
-	// the points on the chessboard
+                                          const std::vector<std::string>& filelist,
+                                          cv::Size & boardSize) {
+    
+    // the points on the chessboard
     std::vector<cv::Point2f> imageCorners;
     std::vector<cv::Point3f> objectCorners;
-
+    
     // 3D Scene Points:
-    // Initialize the chessboard corners 
+    // Initialize the chessboard corners
     // in the chessboard reference frame
-	// The corners are at 3D location (X,Y,Z)= (i,j,0)
-	for (int i=0; i<boardSize.height; i++) {
-		for (int j=0; j<boardSize.width; j++) {
-
-			objectCorners.push_back(cv::Point3f(i, j, 0.0f));
-		}
+    // The corners are at 3D location (X,Y,Z)= (i,j,0)
+    for (int i=0; i<boardSize.height; i++) {
+        for (int j=0; j<boardSize.width; j++) {
+            
+            objectCorners.push_back(cv::Point3f(i, j, 0.0f));
+        }
     }
-
+    
     // 2D Image points:
-    cv::Mat image; // to contain chessboard image
     int successes = 0;
     // for all viewpoints
     for (int i=0; i<filelist.size(); i++) {
-
+        
         // Open the image
-        image = cv::imread(filelist[i],0);
-
+        cv::Mat image = cv::imread(filelist[i],0);
+        if (image.empty()) {
+            cerr << "! image read error: " << filelist[i] << endl;
+            
+        }
         // Get the chessboard corners
         bool found = cv::findChessboardCorners(
-                        image, boardSize, imageCorners);
-
+                                               image, boardSize, imageCorners);
+        
+        if (!found) {
+            cerr << "! Error in findChesboardCorners: " << filelist[i] << endl;
+        }
         // Get subpixel accuracy on the corners
-        cv::cornerSubPix(image, imageCorners, 
-                  cv::Size(5,5), 
-                  cv::Size(-1,-1), 
-			cv::TermCriteria(cv::TermCriteria::MAX_ITER +
-                          cv::TermCriteria::EPS, 
-             30,		// max number of iterations 
-             0.1));     // min accuracy
-
-          // If we have a good board, add it to our data
-		  if (imageCorners.size() == boardSize.area()) {
-
-			// Add image and scene points from one view
+        cv::cornerSubPix(image, imageCorners,
+                         cv::Size(5,5),
+                         cv::Size(-1,-1),
+                         cv::TermCriteria(cv::TermCriteria::MAX_ITER +
+                                          cv::TermCriteria::EPS,
+                                          30,		// max number of iterations
+                                          0.1));     // min accuracy
+        
+        // If we have a good board, add it to our data
+        if (imageCorners.size() == boardSize.area()) {
+            this->imageSize = image.size();
+            // Add image and scene points from one view
             addPoints(imageCorners, objectCorners);
             successes++;
-          }
-
+        }
+        
         //Draw the corners
-        cv::drawChessboardCorners(image, boardSize, imageCorners, found);
-        cv::imshow("Corners on Chessboard", image);
-        cv::waitKey(100);
+        cv::Mat cimage = cv::imread(filelist[i]);
+        srcImages.push_back (cimage.clone());
+        cv::drawChessboardCorners(cimage, boardSize, imageCorners, found);
+        cv::imshow("Corners on Chessboard", cimage);
+        cv::waitKey(200);
     }
-
-	return successes;
+    
+    return successes;
 }
 
 // Add scene points and corresponding image points
 void CameraCalibrator::addPoints(const std::vector<cv::Point2f>& imageCorners, const std::vector<cv::Point3f>& objectCorners) {
-
-	// 2D image points from one view
-	imagePoints.push_back(imageCorners);          
-	// corresponding 3D scene points
-	objectPoints.push_back(objectCorners);
+    
+    // 2D image points from one view
+    imagePoints.push_back(imageCorners);
+    // corresponding 3D scene points
+    objectPoints.push_back(objectCorners);
 }
 
 // Calibrate the camera
 // returns the re-projection error
-double CameraCalibrator::calibrate(cv::Size imageSize)
+double CameraCalibrator::calibrate()
 {
-	// undistorter must be reinitialized
-	mustInitUndistort= true;
+    // undistorter must be reinitialized
+    mustInitUndistort= true;
+    
+    // start calibration
 
-	//Output rotations and translations
-    std::vector<cv::Mat> rvecs, tvecs;
+    flag = flag | CV_CALIB_FIX_ASPECT_RATIO | CV_CALIB_ZERO_TANGENT_DIST | CV_CALIB_FIX_K3;
+    double reprojE = calibrateCamera(objectPoints, // the 3D points
+                                     imagePoints,  // the image points
+                                     imageSize,    // image size
+                                     cameraMatrix, // output camera matrix
+                                     distCoeffs,   // output distortion matrix
+                                     rvecs, tvecs, // Rs, Ts
+                                     flag);        // set options
+    
+    std::cerr << "calibration finished with reproj. error: " << reprojE << std::endl;
+    cerr << "distCoeffs: " << distCoeffs << endl;
+    cerr << "K: " << cameraMatrix << endl;
+    for (int i=0; i<rvecs.size(); i++)
+        cerr << "rv:" << rvecs[i] << endl << "t: " << tvecs[i] << endl;
+    cerr << "-----------" << endl;
 
-	// start calibration
-	return 
-     calibrateCamera(objectPoints, // the 3D points
-		            imagePoints,  // the image points
-					imageSize,    // image size
-					cameraMatrix, // output camera matrix
-					distCoeffs,   // output distortion matrix
-					rvecs, tvecs, // Rs, Ts 
-					flag);        // set options
-//					,CV_CALIB_USE_INTRINSIC_GUESS);
-
+    return reprojE;
 }
 
 // remove distortion in an image (after calibration)
 cv::Mat CameraCalibrator::undistort(const cv::Mat &image) {
-
-	cv::Mat undistorted;
-
-	if (mustInitUndistort) { // called once per calibration
     
-		cv::initUndistortRectifyMap(
-			cameraMatrix,  // computed camera matrix
-            distCoeffs,    // computed distortion matrix
-            cv::Mat(),     // optional rectification (none) 
-			cv::Mat(),     // camera matrix to generate undistorted
-			cv::Size(640,480),
-//            image.size(),  // size of undistorted
-            CV_32FC1,      // type of output map
-            map1, map2);   // the x and y mapping functions
+    cv::Mat undistorted;
+    
+    if (mustInitUndistort) { // called once per calibration
+        
+        cv::initUndistortRectifyMap(
+                                    cameraMatrix,  // computed camera matrix
+                                    distCoeffs,    // computed distortion matrix
+                                    cv::Mat(),     // optional rectification (none)
+                                    cameraMatrix,     // camera matrix to generate undistorted
+                                    image.size(),  // size of undistorted
+                                    CV_32FC1,      // type of output map
+                                    map1, map2);   // the x and y mapping functions
+        
+        mustInitUndistort= false;
+    }
+    
+    // Apply mapping functions
+    cv::remap(image, undistorted, map1, map2, cv::INTER_LINEAR); // interpolation type
+    
+    return undistorted;
+}
 
-		mustInitUndistort= false;
-	}
-
-	// Apply mapping functions
-    cv::remap(image, undistorted, map1, map2, 
-		cv::INTER_LINEAR); // interpolation type
-
-	return undistorted;
+// reproject the obj points through the calibration parameters obtained,
+// to see the appropriateness of the mathematical calibration model.
+void  CameraCalibrator::testReprojection() {
+    cv::Mat_<double> dcoeff = this->distCoeffs;
+    cv::Mat_<double> K = this->cameraMatrix;
+    
+    for (int i=0; i<srcImages.size(); i++) {
+        cv::Mat undistortedImage = this->undistort (srcImages[i]);
+        
+        std::vector<cv::Point3f> &Xw = objectPoints[i];
+        std::vector<cv::Point2f> &p  = imagePoints[i];
+        cv::Mat_<double> Rmat(3,3);
+        cv::Rodrigues (rvecs[i], Rmat);
+        
+        double mean = 0.;
+        for (int k=0; k<p.size(); k++) {
+            // rotation
+            // (3x1) = (3x3) * (3x1)
+            cv::Mat_<double> Xc = Rmat * cv::Mat_<double>(Xw[k]);
+            // translation
+            Xc = Xc + tvecs[i];
+            // perspective projection
+            Xc /= Xc(2);
+            // radial distortion
+            double r2 = pow(Xc(0),2) + pow(Xc(1),2);
+            double r4 = r2*r2;
+            double xd = Xc(0) * (1. + dcoeff(0)*r2 + dcoeff(1)*r4);
+            double yd = Xc(1) * (1. + dcoeff(0)*r2 + dcoeff(1)*r4);
+            double u = K(0,0)*xd + K(0,2);
+            double v = K(1,1)*yd + K(1,2);
+            
+            float xud = K(0,2) + K(0,0)*Xc(0);
+            float yud = K(1,2) + K(1,1)*Xc(1);
+            
+            double eu = u - p[k].x;
+            double ev = v - p[k].y;
+            double err = sqrt( eu*eu + ev*ev );
+//        cerr << "Xc = " << Xc << endl;
+//cerr << "err = " << err << endl;
+            mean += err;
+        
+            cv::circle (undistortedImage, cv::Point2f(xud, yud), 3, cv::Scalar(0,0,255), 2);
+        }
+        cerr << " --- " << mean/p.size() << endl;
+        cv::imshow ("undistorted+reprojection", undistortedImage);
+        cv::waitKey();
+    }
 }
 
 
@@ -140,10 +199,10 @@ cv::Mat CameraCalibrator::undistort(const cv::Mat &image) {
 // 8radialCoeffEnabled should be true if 8 radial coefficients are required (5 is default)
 // tangentialParamEnabled should be true if tangeantial distortion is present
 void CameraCalibrator::setCalibrationFlag(bool radial8CoeffEnabled, bool tangentialParamEnabled) {
-
+    
     // Set the flag used in cv::calibrateCamera()
     flag = 0;
     if (!tangentialParamEnabled) flag += CV_CALIB_ZERO_TANGENT_DIST;
-	if (radial8CoeffEnabled) flag += CV_CALIB_RATIONAL_MODEL;
+    if (radial8CoeffEnabled) flag += CV_CALIB_RATIONAL_MODEL;
 }
 
--- SourceCodes/Chapter 09/calibrate.cpp
+++ SourceCodes/Chapter 09/calibrate.cpp
@@ -1,19 +1,19 @@
 /*------------------------------------------------------------------------------------------*\
-   This file contains material supporting chapter 9 of the cookbook:  
-   Computer Vision Programming using the OpenCV Library. 
-   by Robert Laganiere, Packt Publishing, 2011.
-
-   This program is free software; permission is hereby granted to use, copy, modify, 
-   and distribute this source code, or portions thereof, for any purpose, without fee, 
-   subject to the restriction that the copyright notice may not be removed 
-   or altered from any source or altered source distribution. 
-   The software is released on an as-is basis and without any warranties of any kind. 
-   In particular, the software is not guaranteed to be fault-tolerant or free from failure. 
-   The author disclaims all warranties with regard to this software, any use, 
-   and any consequent failure, is purely the responsibility of the user.
+ This file contains material supporting chapter 9 of the cookbook:
+ Computer Vision Programming using the OpenCV Library.
+ by Robert Laganiere, Packt Publishing, 2011.
  
-   Copyright (C) 2010-2011 Robert Laganiere, www.laganiere.name
-\*------------------------------------------------------------------------------------------*/
+ This program is free software; permission is hereby granted to use, copy, modify,
+ and distribute this source code, or portions thereof, for any purpose, without fee,
+ subject to the restriction that the copyright notice may not be removed
+ or altered from any source or altered source distribution.
+ The software is released on an as-is basis and without any warranties of any kind.
+ In particular, the software is not guaranteed to be fault-tolerant or free from failure.
+ The author disclaims all warranties with regard to this software, any use,
+ and any consequent failure, is purely the responsibility of the user.
+ 
+ Copyright (C) 2010-2011 Robert Laganiere, www.laganiere.name
+ \*------------------------------------------------------------------------------------------*/
 
 #include <iostream>
 #include <iomanip>
@@ -27,50 +27,74 @@
 
 int main()
 {
-
-	cv::namedWindow("Image");
-	cv::Mat image;
-	std::vector<std::string> filelist;
-
-	// generate list of chessboard image filename
-	for (int i=1; i<=20; i++) {
-
-		std::stringstream str;
-		str << "./chessboards/chessboard" << std::setw(2) << std::setfill('0') << i << ".jpg";
-		std::cout << str.str() << std::endl;
-
-		filelist.push_back(str.str());
-		image= cv::imread(str.str(),0);
-		cv::imshow("Image",image);
-	
-		 cv::waitKey(100);
-	}
-
-	// Create calibrator object
+    
+    cv::namedWindow("Image");
+    cv::Mat image;
+    std::vector<std::string> filelist;
+    cv::Size boardSize;
+    
+    // generate list of chessboard image filename
+    for (int i=1; i<=20; i++) {
+        if (1) {
+            boardSize = cv::Size(6,4);
+            std::stringstream str;
+            str << "./chessboards/chessboard" << std::setw(2) << std::setfill('0') << i << ".jpg";
+            std::cout << str.str() << std::endl;
+            
+            filelist.push_back(str.str());
+            image= cv::imread(str.str(),0);
+        } else {
+            boardSize = cv::Size(9,6);
+            char fname[1024];
+            sprintf (fname, "chessboards2/left%02d.jpg", i);
+            image = cv::imread (fname, 0);
+            if (image.empty()) continue;
+            std::cerr << "reading: " << fname << std::endl;
+            filelist.push_back (std::string(fname));
+        }
+        cv::imshow("Image",image);
+        
+        cv::waitKey(100);
+    }
+    
+    // Create calibrator object
     CameraCalibrator cameraCalibrator;
-	// add the corners from the chessboard
-	cv::Size boardSize(6,4);
-	cameraCalibrator.addChessboardPoints(
-		filelist,	// filenames of chessboard image
-		boardSize);	// size of chessboard
-		// calibrate the camera
-    //	cameraCalibrator.setCalibrationFlag(true,true);
-	cameraCalibrator.calibrate(image.size());
+    // add the corners from the chessboard
+    
+    std::cerr << "addChessboardPoints()" << std::endl;
+    
+    cameraCalibrator.addChessboardPoints(
+                                         filelist,	// filenames of chessboard image
+                                         boardSize);	// size of chessboard
+                                                        // calibrate the camera
+                                                        //	cameraCalibrator.setCalibrationFlag(true,true);
+    std::cerr << "now run calibration" << std::endl;
+    
+    cameraCalibrator.calibrate();
+    
+    // display camera matrix
+    cv::Mat cameraMatrix= cameraCalibrator.getCameraMatrix();
+    std::cout << " Camera intrinsic: " << cameraMatrix.rows << "x" << cameraMatrix.cols << std::endl;
+    std::cout << cameraMatrix.at<double>(0,0) << " " << cameraMatrix.at<double>(0,1) << " " << cameraMatrix.at<double>(0,2) << std::endl;
+    std::cout << cameraMatrix.at<double>(1,0) << " " << cameraMatrix.at<double>(1,1) << " " << cameraMatrix.at<double>(1,2) << std::endl;
+    std::cout << cameraMatrix.at<double>(2,0) << " " << cameraMatrix.at<double>(2,1) << " " << cameraMatrix.at<double>(2,2) << std::endl;
 
+    
+    cameraCalibrator.testReprojection();
+    
+    
     // Image Undistortion
-    image = cv::imread(filelist[19]);
-	cv::Mat uImage= cameraCalibrator.undistort(image);
-
-	// display camera matrix
-	cv::Mat cameraMatrix= cameraCalibrator.getCameraMatrix();
-	std::cout << " Camera intrinsic: " << cameraMatrix.rows << "x" << cameraMatrix.cols << std::endl;
-	std::cout << cameraMatrix.at<double>(0,0) << " " << cameraMatrix.at<double>(0,1) << " " << cameraMatrix.at<double>(0,2) << std::endl;
-	std::cout << cameraMatrix.at<double>(1,0) << " " << cameraMatrix.at<double>(1,1) << " " << cameraMatrix.at<double>(1,2) << std::endl;
-	std::cout << cameraMatrix.at<double>(2,0) << " " << cameraMatrix.at<double>(2,1) << " " << cameraMatrix.at<double>(2,2) << std::endl;
-
-    imshow("Original Image", image);
-    imshow("Undistorted Image", uImage);
-
-	cv::waitKey();
-	return 0;
+    for (std::vector<std::string>::iterator it=filelist.begin(); it!=filelist.end(); ++it) {
+        std::cerr << "reading: " << *it << std::endl;
+        image = cv::imread(*it);
+        cv::Mat uImage= cameraCalibrator.undistort(image);
+    
+        imshow("Original Image", image);
+        imshow("Undistorted Image", uImage);
+        
+        cv::waitKey(500);
+    }
+    
+    cv::waitKey();
+    return 0;
 }
